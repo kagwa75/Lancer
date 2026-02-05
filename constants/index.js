9a2d41954e32de8b10a0e0fc457dca67185d88c3
kagwanja <kagwanja@kali.kagwa.com>
2026-02-05 18:30:15 +0300

    second commit

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL = "https://fdddvxvmkeqyufdrvcbc.supabase.co";
+export const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_xImLeAhhXyoXYJJzJUeGtQ_XHvs3Xkf";
+export const stripeKey =
+  "sk_test_51SricTAKaiR3hK9GN9KBtQz4tJp6aMI316VfMxgaWg1u7N7Cd7mrJoVLL94TE5Hfhvq3RRcqgXVQF5obuloHycfb00WFFEeViP";
